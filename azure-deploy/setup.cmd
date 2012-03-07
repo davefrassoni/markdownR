04e9904050e09f6b815922f8fc27ef8cb3825d87
woloski <matias.woloski@southworks.net>
2012-03-07 09:44:42 -0300

    # refactored azure-deploy so that it works on Windows (emulator)

---
+++ azure-deploy/setup.cmd
@@ -0,0 +1,6 @@
+xcopy /E ..\src .\markdown-worker\
+xcopy /E .\startup .\markdown-worker\
+
+@echo ****** The markdown-worker folder contains the azure-friendly version of the source code. You can either use the emulator (Start-AzureEmulator) or publish to Azure (Publish-AzureService) using PowerShell for Node.js ****
+
+@PAUSE
