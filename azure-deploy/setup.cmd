3776a6c1ad7957100d15c556065bcfed6aa8e138
Mariano Vazquez <mariano.vazquez@southworks.net>
2012-03-16 10:37:01 -0300

    # migrated to a web-role

--- azure-deploy/setup.cmd
+++ azure-deploy/setup.cmd
@@ -1,8 +1,8 @@
-xcopy /E ..\src .\markdown-worker\
-del .\markdown-worker\TEMP /S /F /Q
-copy ..\package.json .\markdown-worker\
-xcopy /E .\startup .\markdown-worker\
+xcopy /E ..\src .\markdownr-web\
+del .\markdown-web\TEMP /S /F /Q
+copy ..\package.json .\markdownr-web\
+xcopy /E .\startup .\markdownr-web\bin\
 
-@echo ****** The markdown-worker folder contains the azure-friendly version of the source code. You can either use the emulator (Start-AzureEmulator) or publish to Azure (Publish-AzureService) using PowerShell for Node.js ****
+@echo ****** The markdownr-web folder contains the azure-friendly version of the source code. You can either use the emulator (Start-AzureEmulator) or publish to Azure (Publish-AzureService) using PowerShell for Node.js ****
 
 @PAUSE
