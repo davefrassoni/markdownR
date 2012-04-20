588c1a51a208dc70efd60b77763ecb38fd25f05b
woloski <matias.woloski@southworks.net>
2012-04-20 18:18:03 -0300

    # update install_nodemodules.cmd to use npm 1.1.9

--- azure-deploy/startup/install_nodemodules.cmd
+++ azure-deploy/startup/install_nodemodules.cmd
@@ -5,9 +5,9 @@ echo npm LOG > npmlog.txt
 if "%EMULATED%"=="true" exit /b 0
 
 powershell -c "set-executionpolicy unrestricted"
-powershell .\download.ps1 "http://npmjs.org/dist/npm-1.1.4.zip"
+powershell .\download.ps1 "http://npmjs.org/dist/npm-1.1.9.zip"
 
-7za x npm-1.1.4.zip -y -obin 1>> npmlog.txt 2>> npmlogerr.txt
+7za x npm-1.1.9.zip -y -obin 1>> npmlog.txt 2>> npmlogerr.txt
 bin\npm install 1>> npmlog.txt 2>> npmlogerr.txt
 
 echo SUCCESS
